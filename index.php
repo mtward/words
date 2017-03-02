d657864ea4c46b02b24186dd4738274aeaf9984a
mtward <mtward@users.noreply.github.com>
2017-03-02 10:38:50 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,12 @@
    <title>PHP Test</title>
  </head>
  <body>
-   <?php echo "<p>Hello, World</p>"; ?>
+   <?php 
+  
+$var = file_get_contents('wordlist.txt'); //Take the contents from the file to the variable
+$result = explode(',',$var); //Split it by ','
+echo = $result[array_rand($result)]; //Return a random entry from the array.
+  
+  ?>
  </body>
 </html>
